93185639507042235df1c9f52c5b46705ab79449
Jack Winterschladen <winterschladen.j@gmail.com>
2022-07-31 16:53:38 +0100

    Added method to retrieve which currency was selected from pickerView

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -35,6 +35,11 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return coinManager.currencyArray[row]
     }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedRow = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedRow)
+    }
 
 }
 
