8a4d19201509162d230df11159e30faebc44ce06
Jack Winterschladen <winterschladen.j@gmail.com>
2022-07-31 16:54:43 +0100

    Added getCoinPrice method to get correct rate from currency selected from API.

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,9 +11,34 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "B0FCBCF9-566B-4AD0-AA6A-4E048D20FFC6"
+        
+    func getCoinPrice(for currency: String) {
+        
+        //Use String concatenation to add the selected currency at the end of the baseURL along with the API key.
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        //Use optional binding to unwrap the URL that's created from the urlString
+        if let url = URL(string: urlString) {
+            
+            //Create a new URLSession object with default configuration.
+            let session = URLSession(configuration: .default)
+            
+            //Create a new data task for the URLSession
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                //Format the data we got back as a string to be able to print it.
+                let dataAsString = String(data: data!, encoding: .utf8)
+                print(dataAsString!)
+            }
+            task.resume()
+        }
+    }
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
-    
 }
