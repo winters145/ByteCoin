8ddb6ff2e6fec28dfe5c0b39502192f024055042
Jack Winterschladen <winterschladen.j@gmail.com>
2022-08-01 09:48:21 +0100

    Added CoinManagerDelegate protocol and passed parsed data to view.

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,10 +8,17 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "B0FCBCF9-566B-4AD0-AA6A-4E048D20FFC6"
+    
+    var delegate: CoinManagerDelegate?
         
     func getCoinPrice(for currency: String) {
         
@@ -27,40 +34,45 @@ struct CoinManager {
             //Create a new data task for the URLSession
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
-                    print(error!)
+                    self.delegate?.didFailWithError(error: error!)
                     return
                 }
                 
                 //Format the data we got back as a string to be able to print it.
                     //let dataAsString = String(data: data!, encoding: .utf8)
                 if let safeData = data {
-                    let bitcoinPrice = parseJSON(safeData)
+                    
+                    if let bitcoinPrice = parseJSON(safeData) {
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
                 }
             }
             task.resume()
         }
         
-        func parseJSON(_ data: Data) -> Double? {
+    }
+    func parseJSON(_ data: Data) -> Double? {
+        
+        // Create a JSON decoder
+        let decoder = JSONDecoder()
+        do {
             
-            // Create a JSON decoder
-            let decoder = JSONDecoder()
-            do {
-                
-                // try to decode the data using the CoinData struct
-                let decodedData = try decoder.decode(CoinData.self, from: data)
-                
-                // get the last property from the decoded data
-                let lastPrice = decodedData.rate
-                
-                print(lastPrice)
-                return lastPrice
-                
-            } catch {
-                
-                // catch any errors
-                print(error)
-                return nil
-            }
+            // try to decode the data using the CoinData struct
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            
+            // get the last property from the decoded data
+            let lastPrice = decodedData.rate
+            
+            print(lastPrice)
+            return lastPrice
+            
+        } catch {
+            
+            // catch any errors
+            print(error)
+            return nil
         }
     }
     
