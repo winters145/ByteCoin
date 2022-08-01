d2802431679d5d37528f36145023f2d842e6c463
Jack Winterschladen <winterschladen.j@gmail.com>
2022-08-01 09:19:25 +0100

    Created CoinData Codeable struct for parsing Json data

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Jack Winterschladen on 01/08/2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
